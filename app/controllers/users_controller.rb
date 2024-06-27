99dac022e14665db12da61bf175b8e40a1bab8e1
maeda208 <haneumariderbypornograffitti@yahoo.co.jp>
2024-06-27 10:48:50 +0900

    一覧画面マイページデザイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @stages = user.stages
+    @stages = user.stages.order("created_at DESC")
+    
   end
 end
