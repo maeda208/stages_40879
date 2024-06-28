7045a00f5435c8fcf46f7e57589522e2aa27df3f
maeda208 <haneumariderbypornograffitti@yahoo.co.jp>
2024-06-28 15:55:31 +0900

    ユーザーカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_24_190000) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_28_064609) do
   create_table "stages", charset: "utf8mb4", force: :cascade do |t|
     t.string "reward", null: false
     t.string "title", null: false
@@ -42,6 +42,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_24_190000) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "birth_date"
+    t.integer "age"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
