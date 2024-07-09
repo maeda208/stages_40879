d3efca5e7f8efebb7f62e1ae97bd9b369be86444
maeda208 <haneumariderbypornograffitti@yahoo.co.jp>
2024-07-10 01:22:03 +0900

    追加設定

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -29,16 +29,44 @@ class StagesController < ApplicationController
   def update
     stage = Stage.find(params[:id])
     stage.update(stage_params)
-    redirect_to root_path
+    redirect_to stage_path
   end
 
   def show
     @stage = Stage.find(params[:id])
+    @apply = Apply.new
+    @applies = @stage.applies 
   end
+
+  def participate
+    @stage = Stage.find(params[:stage_id] || params[:id])
+    @apply = Apply.find_by(stage_id: @stage.id, user_id: params[:user_id])
+    @comment = Comment.new
+    @comments = Comment.where(stage_id: @stage.id, apply_id: @apply.id)
+  end
+
+  def pay
+    @stage = Stage.find(params[:id])
+    @apply = Apply.find_by(stage_id: @stage.id, user_id: params[:user_id])
+      redirect_to root_path, alert: '申し込みが見つかりません。'if @apply.nil?
+   end
+
+   def completion
+   end
+   def approval
+    @stage = Stage.find(params[:id])
+    @apply = Apply.find(params[:apply_id])
+    # 承認の処理...
+    @comments = @stage.comments
+    # 処理が終わったら再読み込み
+  end
+
+
+
 
   private
   def stage_params
-    params.require(:stage).permit(:reward, :title, :month_id, :day_id, :show_hour_id, :show_minute_id, :end_hour_id, :end_minute_id, :theater, :address, :conditions).merge(user_id: current_user.id)
+    params.require(:stage).permit(:reward, :title, :month_id, :day_id, :show_hour_id, :show_minute_id, :end_hour_id, :end_minute_id, :theater, :address, :conditions, :start_time).merge(user_id: current_user.id)
   end
 
 end
