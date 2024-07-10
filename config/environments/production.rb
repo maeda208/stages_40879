3a294a1f0db8c94f24dc31b0eee92503d0ff3e1c
maeda208 <haneumariderbypornograffitti@yahoo.co.jp>
2024-07-10 17:36:16 +0900

    メール送信設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,17 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "stages_40879_production"
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                  587,
+  domain:               'gmail.com',
+  user_name:             ENV['SEND_MAIL'],
+  password:              ENV['SEND_MAIL_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto:  true
+}
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
