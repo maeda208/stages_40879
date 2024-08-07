fb0433a4e66ddc82d5f68952bf16c07e116459eb
maeda208 <haneumariderbypornograffitti@yahoo.co.jp>
2024-08-07 18:07:04 +0900

    デプロイ修正

--- db/migrate/20240719143457_change_data_type_for_reward_to_stages.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataTypeForRewardToStages < ActiveRecord::Migration[7.0]
-  def change
-    change_column :stages, :reward, :integer
-  end
-end
--- db/migrate/20240807084217_change_reward_type_in_stages.rb
+++
@@ -1,8 +0,0 @@
-class ChangeRewardTypeInStages < ActiveRecord::Migration[7.0]
-  def change
-    # 現在進行中のカラムの操作をコメントアウトします
-    # remove_column :stages, :reward
-    # add_column :stages, :reward_temp, :integer
-    # rename_column :stages, :reward_temp, :reward
-  end
-end
